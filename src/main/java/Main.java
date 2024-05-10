cfd855720441c98a0869c467e55574e5149d7c5e
Adriel <adriel.fsantos@outlook.com>
2024-05-09 22:35:31 -0300

    feat: Instantiate objects on main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,34 @@
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
+
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
+        Curso curso1 = new Curso(
+                "Curso Java",
+                "Descrição do curso",
+                60
+        );
+        Curso curso2 = new Curso(
+                "Curso Javascript",
+                "Descrição do curso",
+                40
+        );
+        System.out.println(curso1);
+        System.out.println(curso2);
 
+        Mentoria mentoria1 = new Mentoria(
+                "Mentoria de Java",
+                "Descrição da mentoria",
+                LocalDate.now()
+        );
+        Mentoria mentoria2 = new Mentoria(
+                "Mentoria de Python",
+                "Descrição da mentoria",
+                LocalDate.now()
+        );
+        System.out.println(mentoria1);
+        System.out.println(mentoria2);
     }
 }
